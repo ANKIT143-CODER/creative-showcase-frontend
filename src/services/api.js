3cb6176983db6cab5de8c05899924e8cd75dbc40
Ankit Mandal <ankitmandal179914@gmail.com>
2025-12-29 19:57:23 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://creative-showcase-backend-1.onrender.com/api', // backend URL
+  baseURL: 'https://creative-showcase-backend-43.onrender.com/api', // backend URL
 })
 
 export default API
